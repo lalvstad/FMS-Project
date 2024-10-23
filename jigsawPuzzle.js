95c826b39d417b290f95055d26512b8c12aca9e3
Hville <logan.alvstad@gmail.com>
2024-10-23 11:59:43 -0700

    Added sudo code of logic behind jigsawPuzzle

--- jigsawPuzzle.js
+++ jigsawPuzzle.js
@@ -7,3 +7,7 @@ function setup() {
     background('white');
   }
   
+  // Create a graph data structure. Make each piece an object that containes pointers to the correct pieces that fit the sides.
+  // On click, get the piece, and code the movement and placement. When trying to connect, check that the piece connecting to is the correct
+  // pointer for that side.
+  // Graph is complete when a counter of pointer matches is equal to the maximum amount of matches. (When there are no more pointers to check) 
