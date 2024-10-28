0ae3741ee4e2d6cb2d6136cbf2cedb935f09b53b
Naevrys <brankins05@yahoo.com>
2024-10-27 20:12:48 -0700

    Jigsaw Puzzle Prototype

--- jigsawPuzzle.js
+++ jigsawPuzzle.js
@@ -34,11 +34,111 @@
   }
 */
 
+let img;
+let rows = 4;
+let columns = 4;
+let pieces = [];
+let pieceSize;
+let draggingPiece = null;
+let offsetX, offsetY;
+let puzzleCompleted = false;
+
+function preload() {
+  img = loadImage("/images/BunnyPuzzle.png"); 
+}
+
 function setup() {
-  createCanvas(1400, 600);
+  let canvas = createCanvas(800, 800);
+  canvas.parent("PuzzleActivity-container");
+  pieceSize = width / columns;
+  img.resize(width, height);
+  createPuzzlePieces();
+
+  textFont('Silkscreen');
   }
+
+function createPuzzlePieces() {
+  for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+          let x = j * pieceSize;
+          let y = i * pieceSize;
+          let piece = {
+              img: img.get(x, y, pieceSize, pieceSize), 
+              x: random(width - pieceSize),
+              y: random(height - pieceSize),
+              correctX: x,
+              correctY: y,
+              isPlaced: false
+          };
+          pieces.push(piece);
+      }
+  }
+}
   
 function draw() {
   background(220);
-  background('white');
+  for (let piece of pieces) {
+      if (!piece.isPlaced) {
+          image(piece.img, piece.x, piece.y, pieceSize, pieceSize);
+      } else {
+          image(piece.img, piece.correctX, piece.correctY, pieceSize, pieceSize);
+      }
+  }
+
+// Congratulations! Message
+  if (puzzleCompleted) {
+    fill(255, 255, 255, 200); 
+    noStroke();
+    rect(width / 2 - 200, height / 2 - 40, 400, 80, 10);
+
+    fill(0, 150, 0);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text("Congratulations! You've completed the puzzle!", width / 2, height / 2);
+  }
+}
+
+
+function mousePressed() {
+  if (puzzleCompleted) return; 
+
+  for (let piece of pieces) {
+      if (mouseX > piece.x && mouseX < piece.x + pieceSize &&
+          mouseY > piece.y && mouseY < piece.y + pieceSize &&
+          !piece.isPlaced) {
+          draggingPiece = piece;
+          offsetX = mouseX - piece.x;
+          offsetY = mouseY - piece.y;
+          return false;
+      }
+  }
+}
+
+function mouseDragged() {
+  if (draggingPiece) {
+      draggingPiece.x = mouseX - offsetX;
+      draggingPiece.y = mouseY - offsetY;
+  }
+}
+
+function mouseReleased() {
+  if (draggingPiece) {
+      let d = dist(draggingPiece.x, draggingPiece.y, draggingPiece.correctX, draggingPiece.correctY);
+      if (d < pieceSize / 2) {
+          draggingPiece.x = draggingPiece.correctX;
+          draggingPiece.y = draggingPiece.correctY;
+          draggingPiece.isPlaced = true;
+      }
+      draggingPiece = null;
+
+      checkCompletion();
+  }
+}
+
+function checkCompletion() {
+  for (let piece of pieces) {
+    if (!piece.isPlaced) return; 
+  }
+  
+  puzzleCompleted = true; 
 }
